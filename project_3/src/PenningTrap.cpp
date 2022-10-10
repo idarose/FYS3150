2ed14be552a51c881bc27dbdd329497c706ad991
idarose <60488969+idarose@users.noreply.github.com>
2022-10-10 14:45:15 +0200

    Update PenningTrap.cpp

--- project_3/src/PenningTrap.cpp
+++ project_3/src/PenningTrap.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "PenningTrap.hpp"
 #include "Particle.hpp"
+#include <cmath>
 
 // Constructor
 PenningTrap::PenningTrap(double B0_in, double V0_in, double d_in){
@@ -19,7 +20,7 @@ void PenningTrap::add_Particle(Particle p_in){
 
 // External electric field at point r=(x,y,z)
 arma::vec PenningTrap::external_E_field(arma::vec r){
-    arma::vec grad_V;
+    arma::vec grad_V(3);
     grad_V(0) = -2*r(0);
     grad_V(1) = -2*r(1);
     grad_V(2) = 4*r(2);
@@ -38,33 +39,72 @@ arma::vec PenningTrap::external_B_field(arma::vec r){
 arma::vec PenningTrap::force_Particle(int i, int j){
     arma::vec force;
     //neste 2 er feil
-    Particle particle_i = PenningTrap::particles(i);
-    Particle particle_j = PenningTrap::particles(j);
-    arma::vec position_i = particle_i.position_info();
-    arma::vec position_j = particle_j.position_info();
-    arma::vec distance_vector = particle_i.position_info() - particle_j.position_info();
-    force = ke*particle_i.charge() * particle_j.charge() * pow(abs(distance_vector),-3)*distance_vector;
+    Particle particle_i = particles[i];
+    Particle particle_j = particles[j];
+    arma::vec position_i = particle_i.position;
+    arma::vec position_j = particle_j.position;
+    arma::vec distance_vector = particle_i.position - particle_j.position;
+    double mag_distance_vector = sqrt( pow(distance_vector(0),2) + pow(distance_vector(1),2) + pow(distance_vector(2),2));
+    force = ke*particle_i.charge_ * particle_j.charge_ * pow(mag_distance_vector,-3)*distance_vector;
 
     return force;
 };
 
 // The total force on Particle_i from the external fields
 arma::vec PenningTrap::total_force_external(int i){
-    return 0;
+    Particle particle_i = particles[i];
+    arma::vec E_field_force = particle_i.charge_*external_E_field(particle_i.position);
+    arma::vec B_field_force = particle_i.charge_*arma::cross(particle_i.velocity, external_B_field(particle_i.position));
+
+    arma::vec total_force = E_field_force + B_field_force;
+
+    return total_force;
 };
 
-// The total force on Particle_i from the other Particles
+// The total force on Particle_i from the other ALL OTHER Particles
 arma::vec PenningTrap::total_force_Particles(int i){
-    return 0;
+    Particle particle_i = particles[i];
+
+    arma::vec force_particles(3); 
+    int number_particles = particles.size();
+
+    for (int j=0; j<number_particles; j++)
+    {
+        if (i != j)
+        {
+            arma::vec force_j = force_Particle(i,j);
+            force_particles += force_j;
+        }
+    }
+
+    return force_particles;
 };
 
 // The total force on Particle_i from both external fields and other Particles
 arma::vec PenningTrap::total_force(int i){
-    return 0;
+    arma::vec total_force = total_force_external(i) + total_force_Particles(i);
+    return total_force;
 };
 
 // Evolve the system one time step (dt) using Runge-Kutta 4th order
 void PenningTrap::evolve_RK4(double dt){
+
+    int number_particles = particles.size();
+
+    arma::vec a;
+    arma::vec f;
+    arma::vec p;
+
+    arma::mat init; 
+    init.col(0) = particle_j.position;
+    init.col(1) = particle_j.velocity;
+
+    f = {particle_j.position, particle_j.velocity};
+    for (int j=0; j<number_particles; j++){
+        Particle particle_j = particles[j];
+        a = total_force(j)/particle_j.mass_;
+        
+    }
 };
 
 // Evolve the system one time step (dt) using Forward Euler
